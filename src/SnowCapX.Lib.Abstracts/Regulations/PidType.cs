4c4f63ed10b10c7cf03435cf90eace179ccb545c
maSchoeller <info@marvinschoeller.de>
2019-11-14 20:49:59 +0100

    Adding Regulator Abstracts

---
+++ src/SnowCapX.Lib.Abstracts/Regulations/PidType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnowCapX.Lib.Abstracts.Regulations
+{
+    public enum PidType
+    {
+        Standard = 0,
+        Ideal = 1,
+    }
+}
