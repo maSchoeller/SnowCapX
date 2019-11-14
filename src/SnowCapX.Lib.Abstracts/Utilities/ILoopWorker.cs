326e676f50e479a72e4a387f522f62d51f4007ba
maSchoeller <info@marvinschoeller.de>
2019-11-14 20:50:52 +0100

    Adding LoopWorker abstracts

---
+++ src/SnowCapX.Lib.Abstracts/Utilities/ILoopWorker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnowCapX.Lib.Abstracts.Utilities
+{
+    public interface ILoopWorker : IDisposable
+    {
+
+        bool IsRunning { get; }
+        LoopWorkerOptions Options { get; }
+
+        void StartWorker();
+        void StopWorker();
+
+    }
+}
---
+++ src/SnowCapX.Lib.Abstracts/Utilities/LoopWorkerOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnowCapX.Lib.Abstracts.Utilities
+{
+    public class LoopWorkerOptions
+    {
+        public int Delay { get; }
+
+        public LoopWorkerOptions(int delay)
+        {
+            Delay = delay;
+        }
+    }
+}
