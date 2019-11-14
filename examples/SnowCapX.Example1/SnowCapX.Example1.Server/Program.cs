4f4cb73e0f4984d57dc547b20f8bb8809b72b0e4
maSchoeller <info@marvinschoeller.de>
2019-11-14 11:53:34 +0100

    adding example 1

---
+++ examples/SnowCapX.Example1/SnowCapX.Example1.Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnowCapX.Example1.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
