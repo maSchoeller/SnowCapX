6143e7b7f909db97a66621efbd2348626833c507
maSchoeller <info@marvinschoeller.de>
2019-11-15 15:24:35 +0100

    update example1

--- examples/SnowCapX.Example1/SnowCapX.Example1.Server/Program.cs
+++ examples/SnowCapX.Example1/SnowCapX.Example1.Server/Program.cs
@@ -1,12 +1,43 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using SnowCapX.Server.Hosting;
+using SnowCapX.Server.Loops;
 
 namespace SnowCapX.Example1.Server
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CreateHostBuilder(args).Build().Run();
         }
+
+        // Additional configuration is required to successfully run gRPC on macOS.
+        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureDroneHostDefaults(droneBuilder =>
+                {
+                    droneBuilder.ConfigureLoop(loopBuilder =>
+                    {
+                        loopBuilder.Invoke(async (context, next) =>
+                        {
+                            context.Provider.GetService<ILogger<Program>>().LogInformation("Testlogging");
+                            await next();
+                        });
+                    });
+                });
     }
 }
