d9e53cec7b6cad670c4de45b480befa8a31426c2
maSchoeller <info@marvinschoeller.de>
2019-11-15 13:05:28 +0100

    Adding Stabilization and Movements

---
+++ src/SnowCapX.Server/Movements/MovementResolver.cs
@@ -0,0 +1,16 @@
+﻿using SnowCapX.Server.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace SnowCapX.Server.Movements
+{
+    public class MovementResolver : IMovementResolver
+    {
+        public (Vector3 Position, double RotorPower) Convert(Vector3 direction, double speed)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
