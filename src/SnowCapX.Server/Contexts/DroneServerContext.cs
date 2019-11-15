ee4fc11b867582feabc91fb71c7d6c783988658a
maSchoeller <info@marvinschoeller.de>
2019-11-15 13:05:49 +0100

    Adding DroneServerContexts

---
+++ src/SnowCapX.Server/Contexts/DroneServerContext.cs
@@ -0,0 +1,21 @@
+﻿using SnowCapX.Server.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnowCapX.Server.Contexts
+{
+    public class DroneServerContext : IDroneServerContext
+    {
+
+        public DroneServerContext(IMovementSource movementSource, IDroneServerEnviroment enviroment)
+        {
+            MovementSource = movementSource ?? throw new ArgumentNullException(nameof(movementSource));
+            Enviroment = enviroment ?? throw new ArgumentNullException(nameof(enviroment));
+        }
+
+        public IMovementSource MovementSource { get; }
+
+        public IDroneServerEnviroment Enviroment { get; }
+    }
+}
---
+++ src/SnowCapX.Server/Contexts/DroneServerEnviroment.cs
@@ -0,0 +1,11 @@
+﻿using SnowCapX.Server.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnowCapX.Server.Contexts
+{
+    public class DroneServerEnviroment : IDroneServerEnviroment
+    {
+    }
+}
